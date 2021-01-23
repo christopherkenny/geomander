831f8674da85431161dc4f4e893e2d4c3c6bee1d
Christopher Kenny <ctk44@cornell.edu>
2021-01-22 23:52:55 -0500

    Update center.R

--- R/center.R
+++ R/center.R
@@ -12,11 +12,25 @@
 st_centerish <- function(shp){
   cent <- st_centroid(shp)
   
-  outside <- diag(st_within(x = cent, y = shp, spare = FALSE))
   
-  pts <- st_point_on_surface(shp[outside, ])
-  
-  st_geometry(cent[outside,]) <- st_geometry(pts)
+  if(nrow(shp) > 1){
+    outside <- diag(st_within(x = cent, y = shp, spare = FALSE))
+    pts <- st_point_on_surface(shp[outside, ])
+    st_geometry(cent[outside,]) <- st_geometry(pts)
+  } else {
+    
+    
+    outside <- as.logical(st_within(x = cent, y = shp, spare = FALSE))
+    if(is.na(outside)){
+      outside <- TRUE
+    }
+    
+    st_geometry(cent[outside]) <- st_geometry(st_point_on_surface(shp[outside]))
+    
+    
+  }
+
+
   
   return(cent)
 }
