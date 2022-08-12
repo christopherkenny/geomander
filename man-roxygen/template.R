32e8160ee546105ed9ca6d25a6357af39416c54c
Christopher Kenny <ctk44@cornell.edu>
2022-08-12 16:40:52 -0400

    Update template.R

--- man-roxygen/template.R
+++ man-roxygen/template.R
@@ -1,2 +1,2 @@
 #' <%=ifelse(exists("epsg") && epsg, "@param epsg numeric EPSG code to planarize to. Default is 3857.", "") %>
-#' <%=ifelse(exists("tolerance") && tolerance, "@param tolerance postive numeric tolerance to simplify by. Default is 0.01.", "") %>
+#' <%=ifelse(exists("tolerance") && tolerance, "@param tolerance poistive numeric tolerance to simplify by. Default is 0.01.", "") %>
