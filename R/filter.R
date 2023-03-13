8f212efd93402d05f5077734dbb41c7d40db0d8d
Christopher Kenny <ctk44@cornell.edu>
2023-03-13 16:24:55 -0400

    bad escape character in md

--- R/filter.R
+++ R/filter.R
@@ -43,7 +43,7 @@ geo_filter <- function(from, to, bool = FALSE, epsg = 3857) {
 #'
 #' @param from Required. sf dataframe. the geography to subset
 #' @param to Required. sf dataframe. the geography that from must intersect
-#' @param thresh Percent as decimal of an area to trim away. Default is .01, which is 1\%.
+#' @param thresh Percent as decimal of an area to trim away. Default is .01, which is 1%.
 #' @param bool Optional, defaults to FALSE. Should this just return a logical vector?
 #' @templateVar epsg TRUE
 #' @template  template
