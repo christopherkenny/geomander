58b76c49d42f03be218829e660055826cc214983
Christopher Kenny <ctk44@cornell.edu>
2023-03-25 15:13:58 -0400

    Update heda.R

--- R/heda.R
+++ R/heda.R
@@ -121,9 +121,9 @@ get_heda <- function(state, path = tempdir(), clean_names = FALSE, epsg = 3857, 
 #' @return data with cleaned names
 #' @noRd
 clean_heda <- function(data, state) {
-  # custom: az ny co 
+  # custom: az ny co fl
   # auto: ak al ca de ct
-  # todo:  fl ga hi ia id il in ks la ma md mi mn mo ms nc nd ne nh nj nm nv oh ok pa sc sd tn tx vt wa wi wy
+  # todo:   ga hi ia id il in ks la ma md mi mn mo ms nc nd ne nh nj nm nv oh ok pa sc sd tn tx vt wa wi wy
   if (missing(state)) {
     # normal track
     data <- data %>% 
